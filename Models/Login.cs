8d653dd92f45aa74a297a2769a1eddd3d366aa94
joshua <joshuahsientan@gmail.com>
2022-10-27 16:24:59 -0500

    Add project files.

---
+++ Models/Login.cs
@@ -0,0 +1,12 @@
+﻿namespace CoffeeshipRegLabJT.Models
+{
+    public class Login
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
