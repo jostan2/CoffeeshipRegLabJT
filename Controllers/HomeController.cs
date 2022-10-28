fda0546fe10964cb633dd938a885f75fbfe11edf
joshua <joshuahsientan@gmail.com>
2022-10-28 15:55:37 -0500

    update with product list.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ namespace CoffeeshipRegLabJT.Controllers
 {
     public class HomeController : Controller
     {
+        CoffeShopDbContext db = new CoffeShopDbContext();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -31,6 +33,36 @@ namespace CoffeeshipRegLabJT.Controllers
         public IActionResult Result(Login l)
         {
             return View(l);
+        }
+
+        public IActionResult ProductList()
+        {
+            List<Product> products = db.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Product1Details(int Id)
+        {
+            Product product1 = db.Products.Find(Id = 1);
+            return View(product1);
+        }
+
+        public IActionResult Product2Details(int Id)
+        {
+            Product product2 = db.Products.Find(Id = 2);
+            return View(product2);
+        }
+
+        public IActionResult Product3Details(int Id)
+        {
+            Product product3 = db.Products.Find(Id = 3);
+            return View(product3);
+        }
+
+        public IActionResult Product4Details(int Id)
+        {
+            Product product4 = db.Products.Find(Id = 4);
+            return View(product4);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/CoffeShopDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeshipRegLabJT.Models
+{
+    public partial class CoffeShopDbContext : DbContext
+    {
+        public CoffeShopDbContext()
+        {
+        }
+
+        public CoffeShopDbContext(DbContextOptions<CoffeShopDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=CoffeShopDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Category).HasMaxLength(30);
+
+                entity.Property(e => e.Description).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(20);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
